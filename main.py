d74fd3237067b11497706c15cb9def8f1b7042a4
Matvey <zonov90@mail.ru>
2022-06-22 18:00:48 +0300

    1

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-print("check");
-a=87;
-print(f"keka {50*a} drego")
+a=2;
+
+
+def how_many_keks_in_one_drego(number_of_keks):
+    print(f"{number_of_keks} keka consists of {number_of_keks * a} drego");
+    print("Kek is great!")
+
+how_many_keks_in_one_drego(1);
