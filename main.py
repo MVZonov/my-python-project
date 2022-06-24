4b9e322da941a4c81be223bb906810be6427d316
Matvey <zonov90@mail.ru>
2022-06-24 17:42:08 +0300

    1

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-a=2;
-
-
 def how_many_keks_in_one_drego(number_of_keks):
-    print(f"{number_of_keks} keka consists of {number_of_keks * a} drego");
-    print("Kek is great!")
+    print(f"You chose the {number_of_keks > 0} option!")
+    if number_of_keks > 0:
+        return f"{number_of_keks} Keka consists of {number_of_keks * 2} drego\nKek is great!"
+    else:
+        return "Kek is greater than zero! Please try again!"
+users_kek_input = input("Hey Ebo, enter a number of Kek: ")
+users_kek_input_number = int(users_kek_input)
 
-how_many_keks_in_one_drego(1);
+calculated_value = how_many_keks_in_one_drego(users_kek_input_number)
+print(calculated_value)
